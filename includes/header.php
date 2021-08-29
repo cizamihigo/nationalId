810fa49d906e25b9e33799729d2533d26d4a3c7a
cizamihigo <cizamihigochristian@gmail.com>
2021-08-29 15:39:04 +0200

    First Dynamic pages

---
+++ includes/header.php
@@ -0,0 +1,68 @@
+<div class="header" id="home">
+		<div class="content white">
+			<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.html">Politics</a>
+			</div>
+			<!--/.navbar-header-->
+		
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html">Home</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">The Party<b class="caret"></b></a>
+							<ul class="dropdown-menu multi-column columns-2">
+								<div class="row">
+									<div class="party">
+										<ul class="multi-column-dropdown">
+											<li><a href="about.html">History</a></li>
+											<li class="divider"></li>
+											<li><a href="about.html">Philosophy</a></li>
+											<li class="divider"></li>
+											<li><a href="about.html">Photo Gallery</a></li>
+											<li class="divider"></li>
+											<li><a href="typography.html">Short codes</a></li>
+										</ul>
+									</div>
+								</div>
+							</ul>
+						</li>
+						<li><a href="typography.html">Politics</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Leadership<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="leadership.html">Martin</a></li>
+								<li class="divider"></li>
+								<li><a href="leadership.html">Sanders</a></li>
+								<li class="divider"></li>
+								<li><a href="leadership.html">Brush Willium</a></li>
+								<li class="divider"></li>
+								<li><a href="leadership.html">Ben Mark</a></li>
+								<li class="divider"></li>
+								<li><a href="leadership.html">Bruise Alley</a></li>
+							</ul>
+						</li>
+						<li><a href="contact.html">Contact</a></li>
+				</ul>
+				<div class="social-icons">
+					<ul>
+						<li><a href="#" class="facebook"> </a></li>
+						<li><a href="#" class="p"> </a></li>
+						<li><a href="#" class="g"> </a></li>
+						<li><a href="#" class="twitter"> </a></li>
+					</ul>
+				</div>
+			</div>
+				<!--/.navbar-collapse-->
+				<!--/.navbar-->
+			</div>
+			</nav>
+		</div>
+	</div>
