d89f116284bc5be45290fde51970262bb860c240
cizamihigo <cizamihigochristian@gmail.com>
2021-09-01 13:57:58 +0200

    Font design

--- login.php
+++ login.php
@@ -1,29 +1,28 @@
-<link rel="stylesheet" type="text/css" href="css/util.css">
+
 <link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">	
-<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+
 <?php
     $pagename ="Login";
     include_once("head.php");
     include("includes/header.php");
   
 ?>
-<body>
+<body style = "background: white">
+
 	
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-01.png" alt="IMG">
+					<img src="images/1.gif" alt="IMG" hidden="true">
+					<h3><b>Democratic Republic of the Congo</b></h3>
+					<br> <br>
+					<img src="images/2.gif" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form">
 					<span class="login100-form-title">
-						Member Login
+						Citizen Login
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
@@ -57,7 +56,7 @@
 						</a>
 					</div>
 
-					<div class="text-center p-t-136">
+					<div class="text-center p-t-13">
 						<a class="txt2" href="#">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
@@ -70,12 +69,7 @@
 	
 	
 
-	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="vendor/select2/select2.min.js"></script>
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
