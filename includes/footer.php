20b7fbfbf4f28876b256cdd62aef4a2bb4376d65
cizamihigo <cizamihigochristian@gmail.com>
2021-09-01 21:38:27 +0200

    banner placement

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,5 @@
+
+<div class="copyright">
+            <p>&copy; 2021 E-national Id. All Rights Reserved | Design by <a href="aboutus.php"> Mwenelwata KAZUZA SANTA & LOUANGE KAVUGHO MAHITIKO</a> </p>
+</div>
 <script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
--- index.php
+++ index.php
@@ -8,14 +8,70 @@
     <?php
     //session_start();
         include_once("includes/header.php");
-
-
-//
-//
-//
-//
-//
-//
+     ?>  
+     <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){		
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+            });
+        });
+    </script>
+     <div class="banner">
+         <div class="container">
+             <div class="banner-inner">
+                 <div class="callbacks_container">
+                     <ul class="rslides callbacks callbacks1" id="slider4">
+                         <li class="callbacks1_on" style="display: block; float: left; position: relative; opacity: 1; z-index: 2; transition: opacity 500ms ease-in-out;">
+                             <div class="banner-info">
+                             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                             <p>Lorem ipsum dolor sit amet</p>
+                             </div>
+                         </li>
+                         <li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+                             <div class="banner-info">
+                               <h3>Ut enim ad minima veniam, quis nostrum exercitationem</h3>
+                              <p>Lorem ipsum dolor sit amet</p>
+                             </div>
+                         </li>
+                         <li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+                             <div class="banner-info">
+                              <h3>At vero eos et accusamus et iusto odio dignissimos.</h3>
+                             <p>Lorem ipsum dolor sit amet</p>
+                             </div>	
+                         </li>
+                     </ul>
+                 </div>
+                     <!--banner-Slider-->
+                     <script src="js/responsiveslides.min.js"></script>
+                      <script>
+                     // You can also use "$(window).load(function() {"
+                     $(function () {
+                       // Slideshow 4
+                       $("#slider4").responsiveSlides({
+                     auto: true,
+                     pager: true,
+                     nav:false,
+                     speed: 500,
+                     namespace: "callbacks",
+                     before: function () {
+                       $('.events').append("<li>before event fired.</li>");
+                     },
+                     after: function () {
+                       $('.events').append("<li>after event fired.</li>");
+                     }
+                       });
+ 
+                     });
+                       </script>
+             </div>
+         </div>
+   </div>
+   </div>
+   </div>
+   </div>
+ <!--//end-banner--
+     <?php   
        include_once("includes/footer.php");
        
     ?>
