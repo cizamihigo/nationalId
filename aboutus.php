56db6f400bd1441679f182ada0b9b3cf984b9207
cizamihigo <cizamihigochristian@gmail.com>
2021-09-05 23:23:18 +0200

    say something

--- aboutus.php
+++ aboutus.php
@@ -2,7 +2,46 @@
     $pagename = "About-us";
     include('head.php');
     include("includes/header.php");
+?>
+<div class="contact">
+		<div class="container">
+			<h3>How To Find Us</h3>
+			<div class="contact-bottom">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2564.958900464012!2d36.23097800000001!3d49.993379999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a0f009ab9f07%3A0xa21e10f67fa29ce!2sGeorgia+Education+Center!5e0!3m2!1sen!2sin!4v1436943860334" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+			<div class="col-md-4 contact-left">
+				<h4>Address</h4>
+				<p>est eligendi optio cumque nihil impedit quo minus id quod maxime
+					<span>26 56D Rescue,US</span></p>
+				<ul>
+					<li>Free Phone :+1 078 4589 2456</li>
+					<li>Telephone :+1 078 4589 2456</li>
+					<li>Fax :+1 078 4589 2456</li>
+					<li><a href="mailto:info@example.com">info@example.com</a></li>
+				</ul>
+			</div>
+			<div class="col-md-8 contact-left">
+				<h4>Contact Form</h4>
+				<form>
+					<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+					<input type="email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
+					<input type="text" value="Telephone" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Telephone';}" required="">
+					<textarea type="text"  onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message...';}" required="">Message...</textarea>
+					<input type="submit" value="Submit" >
+					<input type="reset" value="Clear" >
 
+				</form>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!-- //contact -->
+
+
+				
+
+
+<?php
     include("includes/footer.php");
 
 ?>
--- requirement.php
+++ requirement.php
@@ -2,7 +2,27 @@
     $pagename = "Requirements";
     include('head.php');
     include("includes/header.php");
-
-    include("includes/footer.php");
-
+?>
+<div class="typo">
+    <div class="container">
+    
+<div class="grid_2">
+    <h3 style="text-align: center">Requirements before Applying</h3> <br>
+    <div class="well">
+        For requesting National Id One should first be a congolese by <em>birth</em> or after <u>changing nationality</u> and should have in his possession documents testifying him to be a Congolese.
+        <ul>
+            <li>Previous Congolese Id,Previous or current passport, Birth certificate showing clearly congolese nationality, Nationality change cerrtificate, Other kind of documents</li>
+        </ul>
+    </div>
+    <div class="well">
+        After having those document and notifying them, a PDF copy containing all the document the owner possess should be established and submitted via the portal. the document <b>should not exceed 2MG</b> <br>For profile picture it should be of the following formats: jepg, png, jpg only. other types won't be accepted. The background should be white and dimesions should be <em>600X600 pixels </em> <br>
+    </div>
+    <div class="well">
+            Our professional team is ready and working 24/7 to catter any single issue you may have using the system. So in order you request to be submitted and analysed it may take up to 5 days.
+    </div>
+</div>
+</div>
+</div>
+<?php
+include("includes/footer.php");
 ?>
